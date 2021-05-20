9e0a15d11caa312a8a9b15170be545606104b580
tiaoh <tiaoh@DESKTOP-EK5VSO0>
2021-05-20 20:10:47 -0300

    Exercicios dia da pessoa em dias.

---
+++ ExerciciosJavaIniciante/src/ExerciciosJava/ProjetoEmpregoNovo.java
@@ -0,0 +1,18 @@
+package ExerciciosJava;
+
+public class ProjetoEmpregoNovo {
+
+	public static void main(String[] args) {
+
+		
+		int idadePessoa = 730;
+		
+		
+		System.out.println("Anos: " + (idadePessoa - 728));
+		System.out.println("Mes: " + (idadePessoa - 706));
+		System.out.println("Dias: " + idadePessoa);
+		 
+		
+		
+	}
+}
