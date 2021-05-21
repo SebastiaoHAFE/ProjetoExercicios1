26246f9f2a81f1b64f44139179afae26d2a80868
tiaoh <tiaoh@DESKTOP-EK5VSO0>
2021-05-20 22:33:55 -0300

    soma de 2 valores.

--- ExerciciosJavaIniciante/src/ExerciciosJava/ProjetoEmpregoNovo.java
+++ ExerciciosJavaIniciante/src/ExerciciosJava/ProjetoEmpregoNovo.java
@@ -1,18 +1,28 @@
-package ExerciciosJava;
+  package  ExerciciosJava ;
 
-public class ProjetoEmpregoNovo {
+public  class  ProjetoEmpregoNovo {
 
-	public static void main(String[] args) {
+	public  static  void  main ( String [] args ) {
 
 		
-		int idadePessoa = 730;
+		int idadePessoa =  730 ;
 		
 		
-		System.out.println("Anos: " + (idadePessoa - 728));
-		System.out.println("Mes: " + (idadePessoa - 706));
-		System.out.println("Dias: " + idadePessoa);
+		System.out.println ( " Anos: "  + (idadePessoa -  728 ));
+		System.out.println ( " Mes: "  + (idadePessoa -  706 ));
+		System.out.println ( " Dias: "  + idadePessoa);
 		 
+		System.out.println("-----------------------------------------------------------");
 		
+		int A = 30;
+		 int B = 10;
+		 
+		 int soma = 30 + 10;
+		 
+		 System.out.print("SOMA = "+ soma + " fim de linha");
 		
+		 
+			System.out.println("-----------------------------------------------------------");
+
 	}
 }
