1c8f30bca21f789b05e040e88401748af5abb8e7
tiaoh <tiaoh@DESKTOP-EK5VSO0>
2021-05-21 21:22:26 -0300

    Projeto Media do Aluno.

--- ExerciciosJavaIniciante/src/ExerciciosJava/ProjetoEmpregoNovo.java
+++ ExerciciosJavaIniciante/src/ExerciciosJava/ProjetoEmpregoNovo.java
@@ -4,6 +4,7 @@ public  class  ProjetoEmpregoNovo {
 
 	public  static  void  main ( String [] args ) {
 
+		/*Projeto Idade da Pessoa em dias*/
 		
 		int idadePessoa =  730 ;
 		
@@ -19,10 +20,32 @@ public  class  ProjetoEmpregoNovo {
 		 
 		 int soma = 30 + 10;
 		 
-		 System.out.print("SOMA = "+ soma + " fim de linha");
+		 System.out.println("SOMA = "+ soma + " fim de linha");
 		
 		 
 			System.out.println("-----------------------------------------------------------");
 
+			/*Projeto Media do Aluno*/
+			
+			double nota1 = 30;
+			double nota2 = 40;
+			double nota3 = 30;
+			double nota4 = 50;
+			
+			double media = (nota1 + nota2 + nota3 + nota4) / 4;
+			
+			if(media >= 70) {
+				System.out.println("O Aluno esta Aprovado " + media);
+			}else if(media >= 40 && media <= 69) {
+				System.out.println("O Aluno ficou em Recuperação " + media);
+			}else {
+				System.out.println("O Aluno Foi Reprovado " + media);
+			
+			}
+	
+	
+	
+	
+	
 	}
 }
